8caf197a61cee48cc5a6ee20125f7234de1f7d2b
Le Nguyen <ltnguyen517@gmail.com>
2023-08-10 09:50:58 -0700

    working on about section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,10 +43,6 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
-          </p>
         </Link>
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
